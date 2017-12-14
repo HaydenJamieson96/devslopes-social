e0e6930077565cab05de10a511e2c0cd398b68fa
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-12-14 17:05:13 +0000

    UI set up

--- devslopes-social/View/CirlceView.swift
+++ devslopes-social/View/CirlceView.swift
@@ -1,5 +1,5 @@
 //
-//  FancyView.swift
+//  CirlceView.swift
 //  devslopes-social
 //
 //  Created by TheAppExperts on 12/14/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FancyView: UIView {
+class CirlceView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -16,8 +16,14 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
     }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
+        
+    
 
 }
