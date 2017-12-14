0b5bc0d55f234b955caaa597b3511bde0f3f7429
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-12-14 10:56:46 +0000

    Sign in UI custom class complete
    Completed the custom views for the sign in UI

---
+++ devslopes-social/Util/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by TheAppExperts on 12/14/17.
+//  Copyright © 2017 TheAppExperts. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
