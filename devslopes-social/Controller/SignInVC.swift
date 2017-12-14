39790b57f449f386675e0350e9c5cf08c5169bcf
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-12-14 12:05:52 +0000

    Facebook auth integrated
    Firebase and Facebook integrated

--- devslopes-social/Controller/SignInVC.swift
+++ devslopes-social/Controller/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,31 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("HAYDEN: UNABLE TO AUTHENTICATE WITH FB - \(String(describing: error))")
+            } else if result?.isCancelled == true{
+                print("HAYDEN: User cancelled fb auth")
+            } else {
+                print("HAYDEN: Successfully auth with FB")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("HAYDEN: UNable to auth with Firebase - \(String(describing: error))")
+            } else {
+                print("HAYDEN: Sucessfully authenticated with Firebase")
+            }
+        }
+    }
+    
 }
 
